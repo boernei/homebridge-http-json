05d904b55aa82090b0b0cfc163566fec11ec1826
Bernhard Hering <hering.bernhard@gmail.com>
2021-03-11 09:49:45 +0100

    set to resol v bus structur

--- index.js
+++ index.js
@@ -1,82 +1,68 @@
 var Service, Characteristic;
 var superagent = require('superagent');
 
-module.exports = function(homebridge){
-  Service = homebridge.hap.Service;
-  Characteristic = homebridge.hap.Characteristic;
-  homebridge.registerAccessory("homebridge-http-json", "http-json", HttpAccessory);
+module.exports = function (homebridge) {
+    Service = homebridge.hap.Service;
+    Characteristic = homebridge.hap.Characteristic;
+    console.log("Hallo")
+    homebridge.registerAccessory("homebridge-http-json", "http-json", HttpAccessory);
 }
 
 function HttpAccessory(log, config) {
-	this.log = log;
+    console.log("Init")
+    this.log = log;
 
-	this.url = config["url"];
-	this.service = config["service"];
-	this.name = config["name"];
-  this.sensors = config["sensors"];
+    this.url = config["url"];
+    this.service = config["service"];
+    this.name = config["name"];
+    this.sensors = config["sensors"];
+    console.log(this.url)
+    console.log(this.sensors)
 }
 
 HttpAccessory.prototype = {
-  getTemperature: function(callback) {
-    this.log("Temperature Triggered");
+    
+    getServices: function () {
+        this.log("getServices")
+        var informationService = new Service.AccessoryInformation();
 
-    superagent.get(this.url).end(function(err, res){
-      if (res.body.temperature) {
-        callback(null, res.body['temperature']);
-      } else {
-        callback(null, null);
-      }
-    });
-  },
-  getHumidity: function(callback) {
-    this.log("Humidity Triggered");
-    superagent.get(this.url).end(function(err, res){
-      if (res.body.humidity) {
-        callback(null, res.body['humidity']);
-      } else {
-        callback(null, null);
-      }
-    });
-  },
-	identify: function(callback) {
-		this.log("Identify requested!");
-		callback();
-	},
+        informationService
+            .setCharacteristic(Characteristic.Manufacturer, "Nuno Ferro")
+            .setCharacteristic(Characteristic.Model, "HTTP JSON")
+            .setCharacteristic(Characteristic.SerialNumber, "ACME#1")
 
-	getServices: function() {
-    this.log("getServices")
-    var informationService = new Service.AccessoryInformation();
+        if (this.service == "Thermostat") {
 
-    informationService
-      .setCharacteristic(Characteristic.Manufacturer, "Nuno Ferro")
-      .setCharacteristic(Characteristic.Model, "HTTP JSON")
-      .setCharacteristic(Characteristic.SerialNumber, "ACME#1")
+            var services = [informationService];
 
-		if (this.service == "Thermostat") {
+            for (var i = this.sensors.length - 1; i >= 0; i--) {
+                let sensor = this.sensors[i];
+                let url = this.url;
+                this.log("Setting up: " + sensor.name);
 
-      var services = [informationService];
+                newService = new Service[sensor.service](sensor.name);
+                newService.getCharacteristic(Characteristic[sensor.caractheristic])
+                    .on('get', function (callback) {
+                        superagent.get(url).end(function (err, res) {
+                            var found = false
+                            res.body.forEach(function(element) {
+                                console.log(element["name"]);
+                                if (element["name"] == sensor.field) {
+                                    callback(null, element["rawValue"]);
+                                    found = true
+                                }
+                            });
 
-      for (var i = this.sensors.length - 1; i >= 0; i--) {
-        let sensor = this.sensors[i];
-        let url = this.url;
-        this.log("Setting up: " + sensor.name);
+                            if (!found) {
+                                callback(null, null);
+                            }
 
-        newService = new Service[sensor.service](sensor.name);
-        newService.getCharacteristic(Characteristic[sensor.caractheristic])
-          .on('get', function(callback) {
-            console.log(sensor.name + " Triggered");
-            superagent.get(url).end(function(err, res){
-              if (res && res.body[sensor.field]) {
-                callback(null, res.body[sensor.field]);
-              } else {
-                callback(null, null);
-              }
-            });
-          })
-        services.push(newService);
-      }
+                        });
+                    })
+                services.push(newService);
+            }
 
-			return services;
-		}
-	}
+            return services;
+        }
+    }
 };
